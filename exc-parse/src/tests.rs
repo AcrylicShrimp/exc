62b54b7d97b8e96d3977fb2dc24cf018b15d6eb9
AcrylicShrimp <led789zxpp@naver.com>
2023-12-02 03:34:31 +0900

    refactor: split test code

--- exc-parse/src/tests.rs
+++ exc-parse/src/tests.rs
@@ -1,1 +1,25 @@
 mod stmt_use;
+mod module_item_use;
+
+use crate::{parse_module, token_iter, ASTModule, NodeIdAllocator};
+use exc_diagnostic::DiagnosticsSender;
+use exc_span::SourceMap;
+use std::{path::PathBuf, sync::mpsc};
+use tokio::task::spawn_blocking;
+
+async fn parse_module_for_test(content: impl AsRef<str>) -> ASTModule {
+    let mut source_map = SourceMap::new();
+    let file = source_map.add_source_file(content.as_ref(), "test.exc", None::<PathBuf>);
+    let (sender, receiver) = mpsc::channel();
+    let diagnostics = DiagnosticsSender::new(file.clone(), sender);
+    let token_stream = token_iter(&file);
+    let mut id_allocator = NodeIdAllocator::new();
+
+    spawn_blocking(move || {
+        while let Ok(diagnostic) = receiver.recv() {
+            eprintln!("{:?}", diagnostic);
+        }
+    });
+
+    parse_module(token_stream, &mut id_allocator, &diagnostics)
+}
---
+++ exc-parse/src/tests/module_item_use.rs
@@ -0,0 +1,48 @@
+use crate::{tests::parse_module_for_test, ASTModuleItemKind};
+
+#[tokio::test]
+async fn test_module_item_use() {
+    const CONTENT: &'static str = "
+    use path;
+    use path::subpath;
+    use path::subpath::subsubpath;
+    use path::subpath::subsubpath as alias;
+    use path::{subpath, subpath2};
+    use path::{subpath, subpath2,};
+    use path::{subpath, subpath2 as alias};
+    use path::{subpath, subpath2 as alias,};
+    use path::*;
+    use path::{subpath, subpath2, *};
+    use path::{subpath, subpath2, *,};
+    use path::{subpath, subpath2 as alias, *};
+    use path::{subpath, subpath2 as alias, *,};
+    use path::{subpath, subpath2 as alias, *, subpath3::{subsubpath, subsubpath2 as alias2, *}};
+    use path::{subpath, subpath2 as alias, *, subpath3::{subsubpath, subsubpath2 as alias2, *},};
+    use path::{};
+
+    pub use path;
+    pub use path::subpath;
+    pub use path::subpath::subsubpath;
+    pub use path::subpath::subsubpath as alias;
+    pub use path::{subpath, subpath2};
+    pub use path::{subpath, subpath2,};
+    pub use path::{subpath, subpath2 as alias};
+    pub use path::{subpath, subpath2 as alias,};
+    pub use path::*;
+    pub use path::{subpath, subpath2, *};
+    pub use path::{subpath, subpath2, *,};
+    pub use path::{subpath, subpath2 as alias, *};
+    pub use path::{subpath, subpath2 as alias, *,};
+    pub use path::{subpath, subpath2 as alias, *, subpath3::{subsubpath, subsubpath2 as alias2, *}};
+    pub use path::{subpath, subpath2 as alias, *, subpath3::{subsubpath, subsubpath2 as alias2, *},};
+    pub use path::{};
+";
+
+    let ast = parse_module_for_test(CONTENT).await;
+    assert_eq!(ast.items.len(), 32);
+
+    match ast.items[0].kind {
+        ASTModuleItemKind::Use(_) => {}
+        _ => panic!("expected use module item"),
+    }
+}
