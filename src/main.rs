81eccb7d4beed6329977c3ea53a9500cb9d3509e
AcrylicShrimp <led789zxpp@naver.com>
2023-12-03 17:11:53 +0900

    feat: add cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,91 @@
 mod write_diagnostic;
 
-use exc_diagnostic::DiagnosticsSender;
-use exc_parse::{parse_module, token_iter, NodeIdAllocator};
-use exc_resolve::{resolve_modules, Module, ModuleASTKind, Visibility};
-use exc_span::SourceMap;
-use std::{path::PathBuf, sync::mpsc};
-use tokio::task::spawn_blocking;
-use write_diagnostic::write_diagnostic;
+#[cfg(test)]
+mod tests;
+
+use clap::{Arg, ArgMatches, Command};
+use colored::Colorize;
+use exc_resolve::{resolve_modules, SourceFileResolver};
+use std::path::PathBuf;
+use thiserror::Error;
+
+fn cli() -> Command {
+    Command::new("exc")
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("build").about("Builds a project").arg(
+                Arg::new("INPUT")
+                    .help("The path to the root module to build")
+                    .required(true)
+                    .index(1)
+                    .value_parser(clap::value_parser!(PathBuf)),
+            ),
+        )
+}
 
 #[tokio::main]
 async fn main() {
-    const CONTENT: &'static str = "
-        module foo {
-            use super::test::{*, foo};
-            use bar::foo;
+    let matches = cli().get_matches();
 
-            module bar {
-                fn foo() {}
+    match matches.subcommand() {
+        Some(("build", matches)) => match build(matches).await {
+            Ok(_) => {}
+            Err(err) => {
+                eprintln!(
+                    "{} Failed to build project: {}",
+                    "[FATAL]".bold().red(),
+                    err
+                );
+                return;
             }
-        }
+        },
+        _ => unreachable!(),
+    };
+}
 
-        module test {
-            fn main() {
-                foo();
-            }
+#[derive(Error, Debug)]
+enum BuildError {
+    #[error("io error: {0}")]
+    IOError(#[from] std::io::Error),
+    #[error("path `{0}` does not exist or is not accessible")]
+    PathNotReachable(PathBuf),
+    #[error("path `{0}` is not a file")]
+    PathIsNotFile(PathBuf),
+    #[error("path `{0}` is the root path; it must have a parent directory")]
+    PathIsRoot(PathBuf),
+    #[error("path `{0}` has no file name")]
+    PathHasNoFileName(PathBuf),
+    #[error("{0}")]
+    SourceFileResolveError(#[from] exc_resolve::SourceFileResolveError),
+}
 
-            fn foo() {
+async fn build(arg: &ArgMatches) -> Result<(), BuildError> {
+    let input = arg.get_one::<PathBuf>("INPUT").unwrap();
+    let absolute_path = std::env::current_dir()?.join(input);
 
-            }
-        }
-    ";
+    if !absolute_path.exists() {
+        return Err(BuildError::PathNotReachable(absolute_path));
+    }
 
-    let mut source_map = SourceMap::new();
-    let file = source_map.add_source_file(CONTENT, "test.exc", Some(PathBuf::from("test.exc")));
-    let (sender, receiver) = mpsc::channel();
-    let diagnostics = DiagnosticsSender::new(file.clone(), sender);
-    let token_stream = token_iter(&file);
-    let mut id_allocator = NodeIdAllocator::new();
+    let absolute_path = absolute_path.canonicalize()?;
 
-    spawn_blocking(move || {
-        while let Ok(diagnostic) = receiver.recv() {
-            write_diagnostic(&diagnostic);
-        }
-    });
+    if !std::fs::metadata(&absolute_path)?.is_file() {
+        return Err(BuildError::PathIsNotFile(absolute_path));
+    }
 
-    let ast = parse_module(token_stream, &mut id_allocator, &diagnostics);
-    let module = Module::new(
-        Visibility::Public,
-        ModuleASTKind::Module(ast.into()),
-        "test.exc",
-        file.clone(),
-        diagnostics,
-    )
-    .unwrap();
+    let root_path = absolute_path
+        .parent()
+        .ok_or_else(|| BuildError::PathIsRoot(absolute_path.clone()))?;
+    let file_name = absolute_path
+        .file_name()
+        .ok_or_else(|| BuildError::PathHasNoFileName(absolute_path.clone()))?;
 
-    resolve_modules(vec![{ module }].into_iter());
+    let mut source_file_resolver = SourceFileResolver::new(root_path);
+    let root_module = source_file_resolver.resolve_file(file_name).await?;
+
+    resolve_modules(std::iter::once(root_module));
+
+    Ok(())
 }
