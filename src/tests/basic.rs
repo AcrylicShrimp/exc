704593ecacc2d16bc1bba38559ab067137d90c38
AcrylicShrimp <led789zxpp@naver.com>
2023-12-03 18:36:33 +0900

    test: add more asserts

--- src/tests/basic.rs
+++ src/tests/basic.rs
@@ -9,11 +9,15 @@ async fn test_basic() {
         .join(&relative_root_path)
         .canonicalize()
         .unwrap();
-    let mut source_file_resolver = SourceFileResolver::new(absolute_root_path);
 
+    let mut source_file_resolver = SourceFileResolver::new(absolute_root_path);
     let root_module = source_file_resolver
         .resolve_file("basic.exc")
         .await
         .unwrap();
+
+    assert_eq!(root_module.path.len(), 1);
+    assert_eq!(root_module.path[0].to_str(), "basic");
+
     resolve_modules(&mut source_file_resolver, root_module).await;
 }
